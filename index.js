10a18264917c225e13b34cc0dbfb098692fd7240
LeeThaiAnn <anmuangu2706@gmail.com>
2024-10-11 20:55:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,30 +13,54 @@ client.on('interactionCreate', async interaction => {
     if (commandName === 'thamgiaevent') {
         const tenIngame = interaction.options.getString('teningame');
 
-        const channelToSend = client.channels.cache.get('1291644635688599603 '); // Thay ID_KÊNH_CẦN_GỬI bằng ID của kênh đích
+        // Tạo một webhook mới cho kênh đích
+        const channelToSend = client.channels.cache.get('1291644635688599603'); // Thay ID_KÊNH_CẦN_GỬI bằng ID của kênh đích
         if (channelToSend.type === ChannelType.GuildText) {
             const webhook = await channelToSend.createWebhook(tenIngame, {
-                avatar: 'https://minecraftvn.net/favicon.ico ', // Tùy chỉnh avatar nếu cần
+                avatar: 'https://example.com/avatar.png', // Tùy chỉnh avatar nếu cần
             });
 
+            // Gửi tin nhắn từ webhook
             await webhook.send(`Người chơi ${tenIngame} đã tham gia event!`);
 
+            // Phản hồi lại người dùng trong kênh gốc
             await interaction.reply(`Webhook cho **${tenIngame}** đã được tạo và gửi!`);
         } else {
             await interaction.reply('Kênh đích không hợp lệ!');
         }
     }
 });
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 10000;
+const { REST, Routes } = require('discord.js');
+const commands = [
+  {
+    name: 'thamgiaevent',
+    description: 'Tham gia sự kiện với tên ingame của bạn',
+    options: [
+      {
+        name: 'teningame',
+        type: 3, // TYPE_STRING
+        description: 'Tên ingame của bạn',
+        required: true,
+      },
+    ],
+  },
+];
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const rest = new REST({ version: '10' }).setToken('MTI5NDI1NjU1MzYwMzM2NjkzMg.GWH-dt.MhEZDmQ2KZShSbjScQv1ORJn1sJnsV85csS9HM');
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-});
+(async () => {
+  try {
+    console.log('Bắt đầu đăng ký (/) lệnh.');
 
-client.login(process.env.TOKEN);
+    await rest.put(
+      Routes.applicationGuildCommands('1294256553603366932', '1254430599448494150'),
+      { body: commands },
+    );
+
+    console.log('Đăng ký lệnh thành công.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+client.login('MTI5NDI1NjU1MzYwMzM2NjkzMg.GWH-dt.MhEZDmQ2KZShSbjScQv1ORJn1sJnsV85csS9HM');
