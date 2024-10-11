b02e83c0b0948fb04d4f89d4e42c90371275cd24
LeeThaiAnn <anmuangu2706@gmail.com>
2024-10-11 20:44:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -27,5 +27,16 @@ client.on('interactionCreate', async interaction => {
         }
     }
 });
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
 
 client.login(process.env.TOKEN);
